2cde1e946e65c52996112bde56748843534e2a67
seonghyeon <dolla_@naver.com>
2023-09-02 17:54:43 +0900

    first Commit

---
+++ src/main/java/com/feed/insightfeed/InsightFeedApplication.java
@@ -0,0 +1,13 @@
+package com.feed.insightfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feed/insightfeed/InsightFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feed.insightfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
